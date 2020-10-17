135172bd7b887bce5b1f1065b811a860b0be968b
Admin <ybagasakthi@gmail.com>
2020-10-17 20:12:23 +0700

    timer using channel

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+var messages = make(chan string)
+
+func main() {
+	go stop()
+	timer(messages)
+
+	fmt.Println()
+}
+
+func timer(req chan string) {
+
+	ticker := time.NewTicker(time.Second)
+
+	for {
+		select {
+		case <-req:
+			return
+		case t := <-ticker.C:
+			fmt.Println("TIME", fmt.Sprintf("%d:%d:%d", t.Hour(), t.Minute(), t.Second()))
+		}
+	}
+
+}
+
+func stop() {
+	// reader := bufio.NewReader(os.Stdin)
+	// msg, _ := reader.ReadString('\n')
+	var msg string
+	fmt.Scanln(&msg)
+
+	messages <- msg
 }
